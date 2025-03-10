aecfdaecfb68cf1a0f83c5eb144f220077a10d14
olakoya <ola.koya1@gmail.com>
2025-03-10 18:57:02 +0000

    updates

--- Day1/Pytest_Framework_Notes.py
+++ Day1/Pytest_Framework_Notes.py
@@ -62,23 +62,27 @@ Naming Conventions in pytest framework (One needs to follow this naming conventi
 
 Pytest Fixtures
 ---------------
-fixtures are functions that manage the setup and teardown process for test environments. Fixtures allow you to define code that needs to run before a test (setup) and after a test (teardown), ensuring a controlled and repeatable test environment. They help in avoiding code duplication, keeping test logic clear, and sharing reusable setup logic across multiple tests.
+fixtures are functions that manage the setup and teardown process for test environments. Fixtures allow you to define
+code that needs to run before a test (setup) and after a test (teardown), ensuring a controlled and repeatable test environment.
+They help in avoiding code duplication, keeping test logic clear, and sharing reusable setup logic across multiple tests.
 
 
 Setup
 ------
-This is the preparation phase that occurs before the test runs. It involves allocating resources, setting initial conditions, or configuring external dependencies like databases, files, or networks.
+This is the preparation phase that occurs before the test runs. It involves allocating resources, setting initial conditions,
+or configuring external dependencies like databases, files, or networks.
 
 Teardown
 --------
-This is the cleanup phase that occurs after the test completes, regardless of whether the test passes or fails. It ensures that any resources used during the test are released, cleaned, or reset, such as closing database connections or deleting temporary files.
+This is the cleanup phase that occurs after the test completes, regardless of whether the test passes or fails. It ensures that
+any resources used during the test are released, cleaned, or reset, such as closing database connections or deleting temporary files.
 
 
 Fixture functions are passed as an argument to testMethod.
-fixtures functions also returns value when called which is optional
+Fixtures functions also returns value when called which is optional
 
 
-Scope of fixtures
+Scope of Fixtures
 ------------------
 Function Scope: Fixture is called once per test function.
 Class Scope: Fixture is called once per class, shared by all test methods in that class.
@@ -102,6 +106,11 @@ For better code maintenance we will specify Configurations and fixtures in conft
 In Pytest by default all the testMethods will be passed unless we put assertions (validation point).
 Pytest automatically discovers and loads the `conftest.py` file for configurations and fixtures if it's in the same directory as the test modules. If it's outside the directory, Pytest won't recognize it.
 
+'''
 
+'''
+Another way to run the code is by typing this "pytest -s -v Day1/test_1.py" in the terminal and running it.
 
+Day1 is ==> Test Suit or Module
+test_1.py is ==> Test Case
 '''
--- Day1/test_1.py
+++ Day1/test_1.py
@@ -1,12 +1,12 @@
 import pytest # firstly, import pytest
 
 # Creating a Class
-class TestClass: # Secondly, create a class
-    def testMethod1(self): # Thirdly, inside of the class create a Module (Method)
-        print("This is test Method1") # Fourthly, inside of module create a Test Case
-
-    def testMethod2(self): # 2nd Module (Method) created inside the same Class
-        print("This is test Method2")
+# class TestClass: # Secondly, create a class
+#     def testMethod1(self): # Thirdly, inside of the class create a Module (Method)
+#         print("This is test Method1") # Fourthly, inside of module create a Test Case
+#
+#     def testMethod2(self): # 2nd Module (Method) created inside the same Class
+#         print("This is test Method2")
 
 '''
 Output from the above 2 Test Cases
@@ -23,5 +23,89 @@ test_1.py::TestClass::testMethod2 PASSED                                 [100%]T
 
 ============================== 2 passed in 0.04s ===============================
 
-The above percentage only displays 100% when all the Test Cases in the Class have finished executing
+The above percentage only displays 100% when all the Test Cases inside the Class have finished executing
+'''
+
+# Fixtures (Fixture is a decorator)
+@pytest.fixture() # Calling fixture in pytest
+
+def setup (): # Creating a function and with it self parameter isn't required and passing it as an arguement
+    print("Launching Browser") # Having simple statement inside the function (prerequisite)
+    yield # Teardown
+    print("Closing Browser")
+
+
+
+# Creating a Class and inside we will have 2 Methods
+class TestClass:
+    def testLogin(self,setup): # Method 1
+        print("This is Login Test")
+    def testLogout(self, setup): # Method 2
+        print("This is Logout Test")
+
+'''
+Output when execute line 38
+Testing started at 18:03 ...
+Launching pytest with arguments test_1.py::TestClass --no-header --no-summary -q in /Users/olakoya/Desktop/Pytest/Day1
+
+============================= test session starts ==============================
+collecting ... collected 2 items
+
+test_1.py::TestClass::testLogin PASSED                                   [ 50%]This is Login Test
+
+test_1.py::TestClass::testLogout PASSED                                  [100%]This is Logout Test
+
+
+============================== 2 passed in 0.02s ===============================
+
+'''
+
+'''
+Output for line 39 after adding line 30 and "setup" to line 41 and 43
+Testing started at 18:09 ...
+Launching pytest with arguments test_1.py::TestClass::testLogin --no-header --no-summary -q in /Users/olakoya/Desktop/Pytest/Day1
+
+============================= test session starts ==============================
+collecting ... collected 1 item
+
+test_1.py::TestClass::testLogin Launching Browser
+PASSED                                   [100%]This is Login Test
+
+
+============================== 1 passed in 0.02s ===============================
+
+'''
+
+'''
+Output for line 39 after adding line 34 and line 35
+Testing started at 18:15 ...
+Launching pytest with arguments test_1.py::TestClass::testLogin --no-header --no-summary -q in /Users/olakoya/Desktop/Pytest/Day1
+
+============================= test session starts ==============================
+collecting ... collected 1 item
+
+test_1.py::TestClass::testLogin Launching Browser
+PASSED                                   [100%]This is Login Test
+Closing Browser
+
+
+============================== 1 passed in 0.02s ===============================
+
+'''
+
+'''
+Output for line 43
+Testing started at 18:16 ...
+Launching pytest with arguments test_1.py::TestClass::testLogout --no-header --no-summary -q in /Users/olakoya/Desktop/Pytest/Day1
+
+============================= test session starts ==============================
+collecting ... collected 1 item
+
+test_1.py::TestClass::testLogout Launching Browser
+PASSED                                  [100%]This is Logout Test
+Closing Browser
+
+
+============================== 1 passed in 0.01s ===============================
+
 '''
