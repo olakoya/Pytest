1a3880dbdb02b072dd64518eff5f04ddc407ca11
olakoya <ola.koya1@gmail.com>
2025-04-11 20:51:22 +0100

    Updates in SeleniumGridDemo

--- Day5/SeleniumGridDemo.py
+++ Day5/SeleniumGridDemo.py
@@ -3,13 +3,21 @@ from selenium import webdriver
 
 hub_url = "http://192.168.1.166:4444/wd/hub"
 
-opt = webdriver.ChromeOptions()
-opt.add_experimental_option("detach", True)
+# opt = webdriver.ChromeOptions()
+# opt.add_experimental_option("detach", True)
 
-opt.platform_name = "Mac"
-opt.browser_version = "chrome"
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
 
-driver = webdriver.Remote(command_executor=hub_url, options=opt)
+# opt.platform_name = "Mac"
+# opt.browser_version = "chrome"
+
+options.platform_name = "Mac"
+options.browser_version = "chrome"
+
+# driver = webdriver.Remote(command_executor=hub_url, options=opt)
+
+driver = webdriver.Remote(command_executor=hub_url, options=options)
 
 driver.get("https://www.google.com/")
 print(driver.title)
--- Day5/SeleniumGridDemo2.py
+++ Day5/SeleniumGridDemo2.py
@@ -1,3 +1,6 @@
+'''
+Redefined codes by ChatGPT
+'''
 import time
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
