b77711a95534ac6f06d1b6b3e2462d1746a2e375
olakoya <ola.koya1@gmail.com>
2025-04-12 20:33:06 +0100

    SelenimuGridSessionReport

--- Day5/test_Parallel.py
+++ Day5/test_Parallel.py
@@ -1,23 +1,42 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from Day4.pom_login import driver
 
-
+# Mine is below this code but trying ChatGPT Code below
 class TestTitle:
-    def test_title_chrome(self,setup):
-        driver = setup
-        driver.get("https://www.google.com/")
-        assert driver.title == "Google"
+
+    def test_title_chrome(self, setup):
+        assert "Google" in setup.title
+
+    def test_title_firefox(self, setup):
+        assert "Google" in setup.title
 
     def test_title_edge(self, setup):
-        driver = setup
-        driver.get("https://www.google.com/")
-        assert driver.title == "Google"
+        assert "Google" in setup.title
 
-    def test_title_firefox(self,setup):
-        driver = setup
-        driver.get("https://www.google.com/")
-        assert driver.title == "Google"
+    def test_title_safari(self, setup):
+        assert "Google" in setup.title
 
-    def test_title_safari(self,setup):
-        driver = setup
-        driver.get("https://www.google.com/")
-        assert driver.title == "Google"
+# My Code: It Works
+# class TestTitle:
+#     def test_title_chrome(self,setup):
+#         driver = setup
+#         driver.get("https://www.google.com/")
+#         assert driver.title == "Google"
+#
+#     def test_title_edge(self, setup):
+#         driver = setup
+#         driver.get("https://www.google.com/")
+#         assert driver.title == "Google"
+#
+#     def test_title_firefox(self,setup):
+#         driver = setup
+#         driver.get("https://www.google.com/")
+#         assert driver.title == "Google"
+#
+#     def test_title_safari(self,setup):
+#         driver = setup
+#         driver.get("https://www.google.com/")
+#         assert driver.title == "Google"
