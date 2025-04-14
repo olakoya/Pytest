678031025ce5960faaacbf43812b2c940648d79a
olakoya <ola.koya1@gmail.com>
2025-04-14 19:07:12 +0100

    Docker

---
+++ Day6_Docker/Docker.py
@@ -0,0 +1,38 @@
+'''
+Docker
+-------
+platform ==> package our application with all its dependencies
+package is a lightweight and portable container
+
+Virtualization
+---------------
+Virtualization creates virtual machines (VMs) that simulate entire hardware environments.
+It is very costly.
+Analogy  ⇒  Imagine a hotel. Each VM is like a separate hotel room with its own amenities (like TV, bed, and bathroom). Every room is independent, but duplicating everything increases cost and size.
+
+Containerization
+----------------
+Containerization runs applications in containers that share the host OS but remain isolated.
+Low cost.
+Analogy  ⇒ Think of a shipping container. Each container holds its own cargo (app and dependencies) but shares the ship (host OS) for transport.
+
+
+Key Differences
+----------------
+Virtualisation ==> Multiple OS environemnts on one machine
+Containerisation  ==> Multiple apps share one OS but run independently
+
+
+Note
+-----
+Image ==> Containers
+
+Images are present on Docker hub
+
+All images are mostly linux images
+
+
+
+
+
+'''
